868e5b1c6ac45d4ec66f1a5979ea5edba22910c6
Carlos vilares <car.vilares@gmail.com>
2024-02-16 06:10:59 +0000

    test commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from .views import add_post
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('add_post/', add_post, name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
 from django.views import generic, View
 from .models import Post
 from.forms import CommentForm
@@ -12,22 +14,40 @@ class PostList(generic.ListView):
 
 class PostDetail(View):
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+   def get(self, request, slug, *args, **kwargs):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.filter(approved=True).order_by("-created_on")
+    liked = False
+    if post.likes.filter(id=self.request.user.id).exists():
+        liked = True
 
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked
-                "comment_form": comment_form()
-                
-            },
-        )
+    # Inicialize comment_form aqui
+    comment_form = CommentForm()
+
+    return render(
+        request,
+        "post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "commented": False,
+            "liked": liked,
+            "comment_form": comment_form
+        },
+    )
+
+
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user  # Define o autor como o usuário atual
+            post.save()
+            return redirect('home')  # Redireciona para a página inicial após a adição do post
+    else:
+        form = PostForm()
+    return render(request, 'add_post.html', {'form': form})
