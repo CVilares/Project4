6ab65afcb2b3495b775a9d77cf1b7a976adeccc3
Carlos vilares <car.vilares@gmail.com>
2024-02-16 00:23:44 +0000

    accounts add to templates

--- portosips/settings.py
+++ portosips/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'portosips.urls'
