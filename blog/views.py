57b30daba5cb0c5b7d02f41f92ce676abdeac45b
Carlos vilares <car.vilares@gmail.com>
2024-02-16 11:45:09 +0000

    like button , view, urlr added, template updated

--- blog/views.py
+++ blog/views.py
@@ -78,3 +78,13 @@ class AddPost(LoginRequiredMixin,
 
 
     
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
